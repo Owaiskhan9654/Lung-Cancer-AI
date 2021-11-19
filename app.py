716918fff77c72357390a437bc98592c83654da8
Owais Ahmad <owaiskhan9654@gmail.com>
2021-11-19 18:22:29 +0530

    Exception Handling

--- app.py
+++ app.py
@@ -42,39 +42,44 @@ def landing_page_after_login():
 @app.route('/loading', methods=['GET', 'POST'])
 @login_required
 def loading_page():
-    year_of_diagnosis = request.form['year_of_diagnosis']
-    age_recoded = request.form['age_recoded']
-    sex = request.form['sex']
-    race = request.form['race']
-    site_recoded = request.form['site_recoded']
-    grade = request.form['grade']
-    primary_site_of_cancer = request.form['pri_site_of_cancer']
-    laterality = request.form['laterality']
-    tumor_size = request.form['tumor_size']
-    t_stage = request.form['t_stage']
-    n_stage = request.form['n_stage']
-    m_stage = request.form['m_stage']
-    metastases_bone = request.form['metastases_bone']
-    metastases_brain = request.form['metastases_brain']
-    metastases_lung = request.form['metastases_lung']
-    metastases_liver = request.form['metastases_liver']
-    ml_algoritms = request.form.getlist('mlalgos')
-    df_data = pd.DataFrame(
-        {'Agerecodewith1yearolds': [int(age_recoded)], 'RacerecodeWhiteBlackOther': [int(race)], 'Sex': [int(sex)],
-         'Yearofdiagnosis': [int(year_of_diagnosis)], 'SiterecodeICDO3WHO2008': [int(site_recoded)],
-         'PrimarySite': [int(primary_site_of_cancer)], 'Gradethru2017': [int(grade)], 'Laterality': [int(laterality)],
-         'DerivedAJCCT6thed20042015': [int(t_stage)], 'DerivedAJCCN6thed20042015': [int(n_stage)],
-         'DerivedAJCCM6thed20042015': [int(m_stage)], 'SEERCombinedMetsatDXbone2010': [int(metastases_bone)],
-         'SEERCombinedMetsatDXbrain2010': [int(metastases_brain)],
-         'SEERCombinedMetsatDXliver2010': [int(metastases_liver)],
-         'SEERCombinedMetsatDXlung2010': [int(metastases_lung)], 'CStumorsize20042015': [int(tumor_size)]})
+    try:
+        year_of_diagnosis = request.form['year_of_diagnosis']
+        age_recoded = request.form['age_recoded']
+        sex = request.form['sex']
+        race = request.form['race']
+        site_recoded = request.form['site_recoded']
+        grade = request.form['grade']
+        primary_site_of_cancer = request.form['pri_site_of_cancer']
+        laterality = request.form['laterality']
+        tumor_size = request.form['tumor_size']
+        t_stage = request.form['t_stage']
+        n_stage = request.form['n_stage']
+        m_stage = request.form['m_stage']
+        metastases_bone = request.form['metastases_bone']
+        metastases_brain = request.form['metastases_brain']
+        metastases_lung = request.form['metastases_lung']
+        metastases_liver = request.form['metastases_liver']
+        ml_algoritms = request.form.getlist('mlalgos')
+        df_data = pd.DataFrame(
+            {'Agerecodewith1yearolds': [int(age_recoded)], 'RacerecodeWhiteBlackOther': [int(race)], 'Sex': [int(sex)],
+             'Yearofdiagnosis': [int(year_of_diagnosis)], 'SiterecodeICDO3WHO2008': [int(site_recoded)],
+             'PrimarySite': [int(primary_site_of_cancer)], 'Gradethru2017': [int(grade)], 'Laterality': [int(laterality)],
+             'DerivedAJCCT6thed20042015': [int(t_stage)], 'DerivedAJCCN6thed20042015': [int(n_stage)],
+             'DerivedAJCCM6thed20042015': [int(m_stage)], 'SEERCombinedMetsatDXbone2010': [int(metastases_bone)],
+             'SEERCombinedMetsatDXbrain2010': [int(metastases_brain)],
+             'SEERCombinedMetsatDXliver2010': [int(metastases_liver)],
+             'SEERCombinedMetsatDXlung2010': [int(metastases_lung)], 'CStumorsize20042015': [int(tumor_size)]})
 
-    df_ml_algorithms=pd.DataFrame({'ml_algoritms': ml_algoritms})
+        df_ml_algorithms=pd.DataFrame({'ml_algoritms': ml_algoritms})
 
 
-    df_data.to_csv('static/tempdf.csv',index=False)
-    df_ml_algorithms.to_csv('static/tempml.csv',index=False)
-    return render_template('loading.html',)
+        df_data.to_csv('static/tempdf.csv',index=False)
+        df_ml_algorithms.to_csv('static/tempml.csv',index=False)
+        return render_template('loading.html',)
+    except:
+        all_prediction_data = []
+        return render_template('predict.html', show_error='true', all_prediction_data=all_prediction_data)
+        
 @app.route('/user_details', methods=['GET','POST'])
 @login_required
 def user_page():
@@ -96,49 +101,49 @@ def process():
         os.remove('static/tempdf.csv')
     except:
         return render_template('predict.html', show_error='true', all_prediction_data=all_prediction_data)
+    try:
+        ml_algoritms=list(df_algorithms['ml_algoritms'])
 
-    ml_algoritms=list(df_algorithms['ml_algoritms'])
+        Prediction_list=['No Model Chosen']
+        for i in ml_algoritms:
 
-    Prediction_list=['No Model Chosen']
-    for i in ml_algoritms:
+            if i == 'random_forest':
+                Prediction_list = utils.Random_forest_predict(df_data_predict)
 
-        if i == 'random_forest':
-            Prediction_list = utils.Random_forest_predict(df_data_predict)
+            elif i == 'catboost':
+                Prediction_list = utils.CatBoost_predict(df_data_predict)
 
-        elif i == 'catboost':
-            Prediction_list = utils.CatBoost_predict(df_data_predict)
+            elif i == 'lightgbm':
+                Prediction_list = utils.LightGBM_predict(df_data_predict)
 
-        elif i == 'lightgbm':
-            Prediction_list = utils.LightGBM_predict(df_data_predict)
+            elif i == 'automl':
+                Prediction_list = utils.AutoML_predict(df_data_predict)
 
-        elif i == 'automl':
-            Prediction_list = utils.AutoML_predict(df_data_predict)
+            elif i == 'logisticregression':
+                Prediction_list = utils.Logistic_Regression_predict(df_data_predict)
 
-        elif i == 'logisticregression':
-            Prediction_list = utils.Logistic_Regression_predict(df_data_predict)
+            elif i == 'svm':
+                Prediction_list = utils.SVC_predict(df_data_predict)
 
-        elif i == 'svm':
-            Prediction_list = utils.SVC_predict(df_data_predict)
+            elif i == 'neural_network':
+                Prediction_list = utils.neural_network_predict(df_data_predict)
 
-        elif i == 'neural_network':
-            Prediction_list = utils.neural_network_predict(df_data_predict)
+            elif i == 'knn':
+                Prediction_list = utils.KNN_predict(df_data_predict)
 
-        elif i == 'knn':
-            Prediction_list = utils.KNN_predict(df_data_predict)
+            elif i == 'xgboost':
+                Prediction_list = utils.XGB_predict(df_data_predict)
 
-        elif i == 'xgboost':
-            Prediction_list = utils.XGB_predict(df_data_predict)
+            elif i == 'extratree':
+                Prediction_list= utils.extratree_predict(df_data_predict)
 
-        elif i == 'extratree':
-            Prediction_list= utils.extratree_predict(df_data_predict)
-
-        all_prediction_data.append(Prediction_list)
+            all_prediction_data.append(Prediction_list)
 
 
-    return render_template('predict.html',show_error='false',all_prediction_data=all_prediction_data)
-    '''except:
+        return render_template('predict.html',show_error='false',all_prediction_data=all_prediction_data)
+    except:
         all_prediction_data=[]
-        return render_template('predict.html', show_error='true', all_prediction_data=all_prediction_data)'''
+        return render_template('predict.html', show_error='true', all_prediction_data=all_prediction_data)
 
 @app.route('/logout', methods=['GET','POST'])
 def logout():
